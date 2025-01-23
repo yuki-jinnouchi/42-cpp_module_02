624375323a4f62c7d712e0f5079bd0c362da3e46
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2025-01-23 16:10:15 +0900

    add ex00

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,33 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointInteger(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed(){
+  std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &source) {
+  std::cout << "Copy constructor called" << std::endl;
+  this->fixedPointInteger = source.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &source){
+  std::cout << "Copy assignation operator called" << std::endl;
+  if (this != &source){
+    this->fixedPointInteger = source.getRawBits();
+  }
+  return *this;
+}
+
+
+int Fixed::getRawBits(void) const{
+  std::cout << "getRawBits member function called" << std::endl;
+  return this->fixedPointInteger;
+}
+
+void Fixed::setRawBits(int const raw){
+  std::cout << "setRawBits member function called" << std::endl;
+  Fixed::fixedPointInteger = raw;
+}
---
+++ ex00/Fixed.hpp
@@ -0,0 +1,39 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+/*
+Create a class in Orthodox Canonical Form that represents a fixed-point number:
+• Private members:
+◦ An integer to store the fixed-point number value.
+◦ A static constant integer to store the number of fractional bits. Its value
+will always be the integer literal 8.
+• Public members:
+◦ A default constructor that initializes the fixed-point number value to 0.
+◦ A copy constructor.
+◦ A copy assignment operator overload.
+◦ A destructor.
+◦ A member function int getRawBits( void ) const;
+that returns the raw value of the fixed-point value.
+◦ A member function void setRawBits( int const raw );
+that sets the raw value of the fixed-point number.
+*/
+
+class Fixed {
+  public:
+    Fixed();
+    ~Fixed();
+    Fixed(const Fixed &source);
+    Fixed &operator=(const Fixed &sth);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+  private:
+    int fixedPointInteger;
+    static const int fractionalBits = 8;
+};
+
+
+#endif
